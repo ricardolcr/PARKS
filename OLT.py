ad5c8b5482653dc1a191ce14b75dd1145c04326f
ricardolcr <ricardojcerqueira@gmail.com>
2025-03-12 11:46:45 -0300

    init olt, add IP

--- OLT.py
+++ OLT.py
@@ -1,4 +1,8 @@
-from credentials import user, passw
+from credentials import user_parks, password_parks
 
 class OLT_PARKS():
-    pass
+    
+    def __init__(self, ip_address):
+
+        self.ip_address = ip_address
+    
